f5f98ee81d090fa243b7f20593d95eb7c178d21b
ManavChaudhary999 <999manavchaudhary@gmail.com>
2024-09-18 12:57:27 +0530

    AI Chat implemented - Fixed 2

--- src/lib/firebase/firebaseAdmin.tsx
+++ src/lib/firebase/firebaseAdmin.tsx
@@ -2,7 +2,7 @@ import { initializeApp, ServiceAccount,getApps, getApp, App, cert } from "fireba
 import { getFirestore } from "firebase-admin/firestore";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require("@root/firebase_admin_key.json");
+// const serviceAccount = require("@root/firebase_admin_key.json");
 
 let app: App;
 
@@ -10,17 +10,9 @@ if (!getApps().length) {
     app = initializeApp({
         // credential: cert(serviceAccount),
         credential: cert({
-            // type: process.env.FIREBASE_TYPE,
             projectId: process.env.FIREBASE_PROJECT_ID,
-            // private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
             privateKey: process.env.FIREBASE_PRIVATE_KEY,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            // client_id: process.env.FIREBASE_CLIENT_ID,
-            // auth_uri: process.env.FIREBASE_AUTH_URI,
-            // token_uri: process.env.FIREBASE_TOKEN_URI,
-            // auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-            // client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-            // universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
         }),
     });
 } else {
